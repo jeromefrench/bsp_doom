0e1e39303b1dd45ca7aa871f489576d005efe2fd
jchardin <jerome.chardin@outook.com>
2019-05-27 10:25:21 +0200

    lecture fichier polygone en cours

--- file_main.c
+++ file_main.c
@@ -6,7 +6,7 @@
 /*   By: jchardin <jerome.chardin@outlook.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/26 13:34:03 by jchardin          #+#    #+#             */
-/*   Updated: 2019/05/26 16:43:58 by jchardin         ###   ########.fr       */
+/*   Updated: 2019/05/27 10:23:54 by jchardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ typedef struct					s_myvec
 //indice allow you to build multiple triangles using share vectices
 typedef struct			s_mypolygon
 {
-	t_myvec				*vertex_list;             //liste des vertices
+	t_myvec				*vertex_lst;             //liste des vertex
 	t_myvec				normal;                   //la normal au polygone
-	int					number_of_vertices;       //nombre de vertices
+	int					number_of_vertex;       //nombre de vertex
 	int					number_of_indices;        //nombre d'indices
 	int					*indices;                 //la listes des indices apres triangulasisation
 	struct s_mypolygon	*next;                    //le prochain noeud dans la liste
@@ -48,19 +48,106 @@ typedef struct			s_mywin
 
 }						t_mywin;
 
+t_myvec			*ft_get_and_create_vertex(char *line, int *j)
+{
+	int			k;
+	int			nbr[100];
+	t_myvec		*s_vertex;
+
+	s_vertex = (t_myvec*)malloc(sizeof(t_myvec));
+	*j = *j + 1;
+	while(ft_isdigit(line[*j]) || line[*j] == '-' )
+	{
+		k = 0;
+		nbr[k] = line[*j];
+		*j = *j + 1;
+		k++;
+	}
+	s_vertex->x = ft_atoi(nbr);
+	*j = *j + 1;
+
+	while(ft_isdigit(line[*j]) || line[*j] == '-' )
+	{
+		k = 0;
+		nbr[k] = line[*j];
+		*j = *j + 1;
+		k++;
+	}
+	s_vertex->y = ft_atoi(nbr);
+	*j = *j + 1;
+
+	while(ft_isdigit(line[*j]) || line[*j] == '-' )
+	{
+		k = 0;
+		nbr[k] = line[*j];
+		*j = *j + 1;
+		k++;
+	}
+	s_vertex->z = ft_atoi(nbr);
+	return (s_vertex);
+}
+
+void			ft_add_vertice(t_myvec **vertex_lst, t_myvec vertex_node)
+{
+	if (*vertex_lst == NULL)
+	{
+		vertex_node.next = NULL;
+		*vertex_lst = &vertex_node;
+	}
+	else
+	{
+		vertex_node.next = *vertex_lst;
+		*vertex_lst = &vertex_node;
+	}
+}
+
+void			ft_go_to_first_vertice(char *line, int *j)
+{
+	while(line[*j] != ':' )
+		*j = *j + 1;
+}
+
+t_mypolygone	*ft_create_polygone_node(t_vec *vertex_lst)
+{
+	t_mypolygone	*polygone_node;
+
+	polygone_node = (t_mypolygone*)malloc(sizeof(t_mypolygone));
+	polygone_node->vertex_lst = vertex_lst;
+	return(polygone_node);
+}
+
+void	ft_add_polygone(t_mypolygone **polygone_lst, t_mypolygone polygone_node)
+{
+
+
+}
+
 t_mypolygon		ft_read_the_polygon_file(void)
 {
-	int		fd;
-	char	*line;
+	int				fd;
+	char			*line;
+	t_myvec			vertex_node;
+	t_myvec			*vertex_lst;
+	t_mypolygon		polygon_node
+	t_mypolygon		*polygon_lst;
+	int				j;
 
 	line = NULL;
+	vertex_lst = NULL;
+	polygon_lst = NULL;
 	fd = open("./file_wall", O_RDWR);
 	while(get_next_line(fd, &line))
 	{
-
-
+		j = 0;
+		ft_go_to_first_vertice(line, &j);
+		while(line[j] != ':')
+		{
+			vertex_node = ft_get_vertex(line, &j);
+			ft_add_vertex(&vertex_lst, vertex_node);
+		}
+		polygone_node = ft_create_polygone_node(t_myvec );
+		ft_add_polygone(&polygon_lst, polygon_node);
 	}
-
 }
 
 void	ft_display_the_polygon_list(void)
