6a5a10c439e5cf2ec404faa3f205fbf156fb326d
jchardin <jerome.chardin@outook.com>
2019-05-27 14:07:54 +0200

    Loic

--- Makefile
+++ Makefile
@@ -6,9 +6,9 @@
 #    By: jchardin <jerome.chardin@outlook.com>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/05/26 15:30:53 by jchardin          #+#    #+#              #
-#    Updated: 2019/05/27 09:14:34 by jchardin         ###   ########.fr        #
+#    Updated: 2019/05/27 13:34:14 by jchardin         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 all:
-	gcc *.c
+	gcc *.c libft.a -fsanitize=address -g
--- file_main.c
+++ file_main.c
@@ -6,25 +6,28 @@
 /*   By: jchardin <jerome.chardin@outlook.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/26 13:34:03 by jchardin          #+#    #+#             */
-/*   Updated: 2019/05/27 11:01:15 by jchardin         ###   ########.fr       */
+/*   Updated: 2019/05/27 14:07:14 by jchardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include "libft.h"
 
 typedef struct					s_myvec
 {
 	float						x;
 	float						y;
 	float						z;
+	struct s_myvec				*next;
 }								t_myvec;
 
 //indice allow you to build multiple triangles using share vectices
 typedef struct			s_mypolygon
 {
 	t_myvec				*vertex_lst;             //liste des vertex
-	t_myvec				normal;                   //la normal au polygone
+	t_myvec				normal;                   //la normal au polygon
 	int					number_of_vertex;       //nombre de vertex
 	int					number_of_indices;        //nombre d'indices
 	int					*indices;                 //la listes des indices apres triangulasisation
@@ -44,14 +47,14 @@ typedef struct			s_mynode
 //structure principale
 typedef struct			s_mywin
 {
-	t_mypolygon			polygon_lst;
+	t_mypolygon			*polygon_lst;
 
 }						t_mywin;
 
-t_myvec			*ft_get_and_create_vertex(char *line, int *j)
+t_myvec			*ft_get_and_create_vertex_node(char *line, int *j)
 {
 	int			k;
-	int			nbr[100];
+	char		nbr[100];
 	t_myvec		*s_vertex;
 
 	s_vertex = (t_myvec*)malloc(sizeof(t_myvec));
@@ -87,7 +90,7 @@ t_myvec			*ft_get_and_create_vertex(char *line, int *j)
 	return (s_vertex);
 }
 
-void			ft_add_vertice(t_myvec **vertex_lst, t_myvec *vertex_node)
+void			ft_add_vertex(t_myvec **vertex_lst, t_myvec *vertex_node)
 {
 	if (*vertex_lst == NULL)
 	{
@@ -101,43 +104,43 @@ void			ft_add_vertice(t_myvec **vertex_lst, t_myvec *vertex_node)
 	}
 }
 
-void			ft_go_to_first_vertice(char *line, int *j)
+void			ft_go_to_first_vertex(char *line, int *j)
 {
 	while(line[*j] != ':' )
 		*j = *j + 1;
 }
 
-t_mypolygone	*ft_create_polygone_node(t_vec *vertex_lst)
+t_mypolygon	*ft_create_polygon_node(t_myvec *vertex_lst)
 {
-	t_mypolygone	*polygone_node;
+	t_mypolygon	*polygon_node;
 
-	polygone_node = (t_mypolygone*)malloc(sizeof(t_mypolygone));
-	polygone_node->vertex_lst = vertex_lst;
-	return(polygone_node);
+	polygon_node = (t_mypolygon*)malloc(sizeof(t_mypolygon));
+	polygon_node->vertex_lst = vertex_lst;
+	return(polygon_node);
 }
 
-void	ft_add_polygone(t_mypolygone **polygone_lst, t_mypolygone *polygone_node)
+void	ft_add_polygon(t_mypolygon **polygon_lst, t_mypolygon *polygon_node)
 {
-	if (*polygone_lst == NULL)
+	if (*polygon_lst == NULL)
 	{
-		polygone_node->next = NULL;
-		*polygone_lst = polygone_node;
+		polygon_node->next = NULL;
+		*polygon_lst = polygon_node;
 	}
 	else
 	{
-		polygone_node->next = *polygone_lst;
-		*polygone_lst = polygone_node;
+		polygon_node->next = *polygon_lst;
+		*polygon_lst = polygon_node;
 	}
 }
 
-t_mypolygon		ft_read_the_polygon_file(void)
+void	ft_read_the_polygon_file(t_mypolygon *polygon_lst)
 {
 	int				fd;
 	char			*line;
 	t_myvec			*vertex_node;
 	t_myvec			*vertex_lst;
 	t_mypolygon		*polygon_node;
-	t_mypolygon		*polygon_lst;
+//	t_mypolygon		*polygon_lst;
 	int				j;
 
 	line = NULL;
@@ -147,29 +150,28 @@ t_mypolygon		ft_read_the_polygon_file(void)
 	while(get_next_line(fd, &line))
 	{
 		j = 0;
-		ft_go_to_first_vertice(line, &j);
+		ft_go_to_first_vertex(line, &j);
 		while(line[j] != ':')
 		{
 			vertex_node = ft_get_and_create_vertex_node(line, &j);
-			ft_add_vertex(&vertex_lst, vertex_node);
+			ft_add_vertex(vertex_lst, vertex_node);
 		}
-		polygone_node = ft_create_polygone_node(t_myvec *vertex_lst);
-		ft_add_polygone(&polygon_lst, polygon_node);
+		polygon_node = ft_create_polygon_node(vertex_lst);
+		ft_add_polygon(&polygon_lst, polygon_node);
 	}
-	return (polygon_lst);
 }
 
 void	ft_display_the_polygon_list(t_mypolygon *polygon_lst)
 {
 	t_mypolygon		*keep;
 
-	keep = polygone_lst;
-	while(polygone_lst != NULL)
+	keep = polygon_lst;
+	while(polygon_lst != NULL)
 	{
-		printf("first verrtice =%d\n", polygon_lst->);
-		polygone_lst = polygon->next;
+		printf("first verrtice =%f\n", polygon_lst->vertex_lst->x);
+		polygon_lst = polygon_lst->next;
 	}
-	polygone_lst = keep;
+	polygon_lst = keep;
 }
 
 int		main(void)
@@ -177,7 +179,8 @@ int		main(void)
 	t_mywin		s_win;
 
 	printf("hello chef Inca\n");
-	s_win.polygon_lst = ft_read_the_polygon_file();
+	/*s_win.polygon_lst = */ft_read_the_polygon_file(s_win.polygon_lst);
+	printf("first vetice = %f\n", s_win.polygon_lst->vertex_lst->x);
 	ft_display_the_polygon_list(s_win.polygon_lst);
 	return (1);
 }
