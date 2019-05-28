343a6c969e538685a5f547207ae0cea56b7f701e
jchardin <jerome.chardin@outook.com>
2019-05-28 17:48:54 +0200

    ok

--- file_main.c
+++ file_main.c
@@ -6,7 +6,7 @@
 /*   By: jchardin <jerome.chardin@outlook.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/26 13:34:03 by jchardin          #+#    #+#             */
-/*   Updated: 2019/05/28 17:32:53 by jchardin         ###   ########.fr       */
+/*   Updated: 2019/05/28 17:48:43 by jchardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,19 +88,37 @@ int			ft_calculate_number_of_indices(t_mypolygon *polygon_node)
 
 int			*ft_calculate_indices_tab(t_mypolygon *polygon_node)
 {
+	int		i;
 	int		*indices;
+	int		v0, v1, v2;
 
-	indices = NULL;
-	(void)polygon_node;
+	i = 0;
+	indices = (int*)malloc(sizeof(int) * polygon_node->number_of_indices);
+	while (i < polygon_node->number_of_indices / 3)
+	{
+		if (i == 0)
+		{
+			v0 = 0;
+			v1 = 1;
+			v2 = 2;
+		}
+		else
+		{
+			v1 = v2;
+			v2++;
+		}
+		indices[i * 3] = v0;
+		indices[(i * 3) + 1] = v1;
+		indices[(i * 3) + 2] = v2;
+		i++;
+	}
 	return (indices);
 }
-
-
 
 void		ft_process_polygon(t_mypolygon *polygon_lst)
 {
 	t_mypolygon		*keep;
-	int				i;
+	int				i, j;
 
 	keep = polygon_lst;
 	i = 0;
@@ -115,6 +133,14 @@ void		ft_process_polygon(t_mypolygon *polygon_lst)
 		polygon_lst->number_of_indices = ft_calculate_number_of_indices(polygon_lst);
 		printf("Number of indices =%d\n", polygon_lst->number_of_indices);
 		polygon_lst->indices = ft_calculate_indices_tab(polygon_lst);
+		j = 0;
+		printf("Les indices : ");
+		while (j < polygon_lst->number_of_indices)
+		{
+			printf("%d  ", polygon_lst->indices[j]);
+			j++;
+		}
+		printf("\n");
 		polygon_lst = polygon_lst->next;
 		i++;
 	}
