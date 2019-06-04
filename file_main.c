25b629951dfef6c1827597c98cc149e13bcd2313
jchardin <jerome.chardin@outook.com>
2019-06-04 16:58:24 +0200

    save

--- file_main.c
+++ file_main.c
@@ -6,7 +6,7 @@
 /*   By: jchardin <jerome.chardin@outlook.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/26 13:34:03 by jchardin          #+#    #+#             */
-/*   Updated: 2019/06/04 15:56:06 by jchardin         ###   ########.fr       */
+/*   Updated: 2019/06/04 16:57:51 by jchardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,12 @@ int			main(void)
 	ft_display_the_polygon_list(s_win.polygon_lst);
 	ft_process_polygon(s_win.polygon_lst);
 	ft_build_bsp_tree((&s_node), s_win.polygon_lst);
-	ft_afficher_le_bsp(&s_node);
+	//ft_afficher_le_bsp(&s_node);
+
 
 	ft_display_polygon();
+
+
 
 
 	return (1);
